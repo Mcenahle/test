257986866e3d9b214bd0593320114396f2ab7e52
Mcenahle <admin@mcenahle.sh.cn>
2025-01-13 07:47:18 +0800

    Delete .vitepress/config.js

--- .vitepress/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  themeConfig: {
-    repo: 'mcenahle/test', // GitHub 仓库的 URL
-    docsDir: 'docs', // 文档所在目录，如果不是默认的 `docs` 目录，请更改此值
-    editLinks: true, // 启用编辑链接
-    editLinkText: '在 GitHub 上编辑此页', // 编辑链接的文本
-  },
-};

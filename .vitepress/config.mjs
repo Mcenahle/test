56d938543e8fbd279eb3a91a6c6ec35a212f0808
Mcenahle <admin@mcenahle.sh.cn>
2025-01-13 13:59:35 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,35 +2,52 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/test/",
-  title: "Minecraft Docs",
-  description: "Minecraft Docs",
+  title: "Minecraft Docs Website",
+  description: "Minecraft Docs Website",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-  { text: '主页', link: '/' },
-  { text: 'Examples', link: '/markdown-examples' },
-  {
-    text: "其他网站",
-    items: [
+      { text: '主页', link: '/' },
+      { text: 'Minecraft 官网', link: 'https://www.minecraft.net' },
       {
-        text: "个人主页",
-        link: "https://www.mcenahle.sh.cn",
+        text: "本站目录",
+        items: [
+          {
+            text: "Minecraft 材质包下载与安装",
+            link: "/install-datapacks",
+          },
+          {
+            text: "Minecraft 光影下载与安装",
+            link: "/install-shadowpacks",
+          },
+          {
+            text: "Minecraft 存档下载与安装",
+            link: "/install-saves",
+          },
+        ],
       },
       {
-        text: "个人博客",
-        link: "https://blog.mcenahle.sh.cn",
+        text: "其他网站",
+        items: [
+          {
+            text: "个人主页",
+            link: "https://www.mcenahle.sh.cn",
+          },
+          {
+            text: "个人博客",
+            link: "https://blog.mcenahle.sh.cn",
+          },
+        ],
       },
     ],
-  },
-],
 
     sidebar: [
       {
         text: 'Examples',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'Minecraft 材质包下载与安装', link: '/install-datapacks' },
+          { text: 'Minecraft 光影下载与安装', link: '/install-shadowpacks' },
+          { text: 'Minecraft 存档下载与安装', link: '/install-saves' }
         ]
       }
     ],
